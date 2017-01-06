f8075036ade8d0d410995625124d2d8b1f897310
webservsol <webservsol@gmail.com>
2017-01-06 20:52:31 +0200

    Add files via upload

---
+++ secure.js
@@ -0,0 +1,11 @@
+(function($) {
+	
+	function toggleElements() {
+ 
+	 document.getElementById("revisionsdiv").remove();
+	 
+	 document.getElementsByClassName("vc_templates-button").remove();
+	 
+	}
+	
+})(jQuery);
